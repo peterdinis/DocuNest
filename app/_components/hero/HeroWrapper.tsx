b1b537d99e40f7e25d3eda6e30489b747d126ea6
Peter Dinis <peterdinis0@pobox.sk>
2024-07-13 21:15:39 +0200

    FOO

--- app/_components/hero/HeroWrapper.tsx
+++ app/_components/hero/HeroWrapper.tsx
@@ -2,56 +2,60 @@ import { FC } from "react";
 
 const HeroWrapper: FC = () => {
   return (
-    <div>
-      <img
-        className="absolute w-full object-cover object-left-top h-screen inset-0 top-0 hidden dark:block"
-        src="images/bg.webp"
-        alt="image"
-        loading="lazy"
-      />
-      <img
-        className="absolute w-full object-cover object-top h-screen inset-0 top-0 dark:hidden"
-        src="images/bg2.webp"
-        alt="image"
-        loading="lazy"
-      />
-      <div className="relative container m-auto px-6 md:px-12 lg:px-7">
-        <div className="py-40 lg:py-56 md:w-9/12 lg:w-7/12 dark:lg:w-6/12 ml-auto">
-          <h1 className="text-gray-900 dark:text-white font-bold text-4xl md:text-6xl lg:text-4xl xl:text-6xl">
-            Shaping a world with{" "}
-            <span className="text-teal-600 dark:text-teal-500">
-              reimagination.
-            </span>
-          </h1>
-          <p className="mt-8 text-gray-700 dark:text-gray-300">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
-            incidunt nam itaque sed eius modi error totam sit illum. Voluptas
-            doloribus asperiores quaerat aperiam. Quidem harum omnis beatae
-            ipsum soluta!
-          </p>
-          <div className="mt-16 space-y-2 lg:space-y-0 md:w-max sm:space-x-6">
-            <button
-              type="button"
-              title="Start buying"
-              className="w-full py-3 px-6 text-center rounded-full transition bg-teal-300 hover:bg-teal-100 active:bg-teal-400 focus:bg-teal-300 sm:w-max"
-            >
-              <span className="block text-teal-900 font-semibold text-sm">
-                Contact Us
-              </span>
-            </button>
-            <button
-              type="button"
-              title="Start buying"
-              className="w-full py-3 px-6 text-center rounded-full transition border border-gray-200 dark:active:bg-teal-900 dark:focus:bg-gray-800 active:bg-teal-200 focus:bg-teal-100 sm:w-max"
-            >
-              <span className="block text-teal-800 dark:text-teal-100 font-semibold text-sm">
-                About us
-              </span>
-            </button>
-          </div>
+    <div className="relative pt-40 pb-20 lg:pt-44">
+    <div className="relative xl:container m-auto px-6 md:px-12 lg:px-6">
+        <h1 className="sm:mx-auto sm:w-10/12 md:w-2/3 font-black text-blue-900 text-4xl text-center sm:text-5xl md:text-6xl lg:w-auto lg:text-left xl:text-7xl dark:text-white">Run successful remote and <br className="lg:block hidden" /> <span className="relative text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary dark:from-primaryLight dark:to-secondaryLight">Hybrid teams</span>.</h1>
+        <div className="lg:flex">
+            <div className="relative mt-8 md:mt-16 space-y-8 sm:w-10/12 md:w-2/3 lg:ml-0 sm:mx-auto text-center lg:text-left lg:mr-auto lg:w-7/12">
+                <p className="sm:text-lg text-gray-700 dark:text-gray-300 lg:w-11/12">
+                    DailyBot takes chat and collaboration to the next level: daily standups, team check-ins, surveys, kudos, best companion bot for your virtual watercooler, 1:1 intros, motivation tracking and more.
+                </p>
+                <span className="block font-semibold text-gray-500 dark:text-gray-400">The best companion bot for your chat app.</span>
+                <div className="grid grid-cols-3 space-x-4 md:space-x-6 md:flex md:justify-center lg:justify-start">
+                    <a aria-label="add to slack" href="#" className="p-4 border border-gray-200 dark:bg-gray-800 dark:border-gray-700 rounded-full duration-300 hover:border-cyan-400 hover:shadow-lg hover:shadow-cyan-600/20 dark:hover:border-cyan-300/30">
+                        <div className="flex justify-center space-x-4">
+                            <img className="w-6 h-6" src="images/slack.png" alt="slack logo" loading="lazy" width="128" height="128" />
+                            <span className="hidden font-medium md:block dark:text-white">Slack</span>
+                        </div>
+                    </a>    
+                    <a aria-label="add to chat" href="#" className="p-4 border border-gray-200 dark:bg-gray-800  dark:border-gray-700 rounded-full duration-300 hover:border-green-400 hover:shadow-lg hover:shadow-lime-600/20 dark:hover:border-green-300/30">
+                        <div className="flex justify-center space-x-4">
+                            <img className="w-6 h-6" src="images/chat.png" alt="chat logo" loading="lazy" width="128" height="128" />
+                            <span className="hidden font-medium md:block dark:text-white">Google Chat</span>
+                        </div>
+                    </a>   
+                    <a aria-label="add to zoom" href="#" className="p-4 border border-gray-200 dark:bg-gray-800  dark:border-gray-700 rounded-full duration-300 hover:border-blue-400 hover:shadow-lg hover:shadow-blue-600/20 dark:hover:border-blue-300/30">
+                        <div className="flex justify-center space-x-4">
+                            <img className="w-6 h-6" src="images/zoom.png" alt="chat logo" loading="lazy" width="128" height="128" />
+                            <span className="hidden font-medium md:block dark:text-white">Zoom</span>
+                        </div>
+                    </a>    
+                </div>
+
+                <div className="dark:text-gray-300">
+                    🔥🌟 
+                    <span>Other integrations :</span> 
+                    <a href="#" className="font-semibold text-gray-700 dark:text-gray-200">Discord,</a>
+                    <a href="#" className="font-semibold text-gray-700 dark:text-gray-200">Telegram</a>
+                </div>
+
+                <div className="pt-12 flex gap-6 lg:gap-12 justify-between grayscale lg:w-2/3">
+                    <img src="./images/clients/airbnb.svg" className="h-8 sm:h-10 w-auto lg:h-12" alt="" />
+                    <img src="./images/clients/ge.svg" className="h-8 sm:h-10 w-auto lg:h-12" alt="" />
+                    <img src="./images/clients/coty.svg" className="h-8 sm:h-10 w-auto lg:h-12" alt="" />
+                    <img src="./images/clients/microsoft.svg" className="h-8 sm:h-10 w-auto lg:h-12" alt="" />
+                </div>
+            </div>
+            <div className="mt-12 md:mt-0 lg:absolute -right-10 lg:w-7/12">
+                <div className="relative w-full">
+                    <div aria-hidden="true" className="absolute scale-75 md:scale-110 inset-0 m-auto w-full h-full md:w-96 md:h-96 rounded-full rotate-45 bg-gradient-to-r from-primaryLight to-secondaryLight blur-3xl"></div>
+                    <img src="images/globalization-cuate.svg" className="relative w-full" alt="wath illustration" loading="lazy" width="320" height="280" />
+                </div>
+            </div>
         </div>
-      </div>
     </div>
+</div>
+         
   );
 };
 
