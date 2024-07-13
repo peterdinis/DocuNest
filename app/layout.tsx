fa4b0e4908d356ba1b741ff81e68d192fedd1c21
Peter Dinis <peterdinis0@pobox.sk>
2024-07-13 21:54:08 +0200

    infra: Formatting files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,34 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import NextUiProvider from "./_components/shared/providers/NextUiProvider";
-import Navbar from "./_components/shared/Navbar";
-import ScrollToTop from "./_components/shared/ScrollToTop";
-import ThemeProvider from "./_components/shared/providers/ThemeProvider";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import NextUiProvider from './_components/shared/providers/NextUiProvider';
+import Navbar from './_components/shared/Navbar';
+import ScrollToTop from './_components/shared/ScrollToTop';
+import ThemeProvider from './_components/shared/providers/ThemeProvider';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "DocuNest",
-  description: "Document application with AI power",
+    title: 'DocuNest',
+    description: 'Document application with AI power',
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <NextUiProvider>
-          <ThemeProvider>
-            <Navbar />
-            {children}
-            <ScrollToTop />
-          </ThemeProvider>
-        </NextUiProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body className={inter.className}>
+                <NextUiProvider>
+                    <ThemeProvider>
+                        <Navbar />
+                        {children}
+                        <ScrollToTop />
+                    </ThemeProvider>
+                </NextUiProvider>
+            </body>
+        </html>
+    );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,27 @@
-import { nextui } from "@nextui-org/react";
-import type { Config } from "tailwindcss";
+import { nextui } from '@nextui-org/react';
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: "class",
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+        './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+        },
     },
-  },
-  plugins: [nextui(), require('@tailwindcss/typography'), require("tailwindcss-animate")],
+    plugins: [
+        nextui(),
+        require('@tailwindcss/typography'),
+        require('tailwindcss-animate'),
+    ],
 };
 export default config;
