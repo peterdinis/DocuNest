4bc95b12173b52b3efe3987d4c73e757df823f72
Peter Dinis <peterdinis0@pobox.sk>
2024-07-14 09:11:36 +0200

    feat: Updating landing page WIP

--- app/_components/hero/HeroWrapper.tsx
+++ app/_components/hero/HeroWrapper.tsx
@@ -1,34 +1,36 @@
+'use client';
+
 import { FC } from 'react';
+import { Button } from '@nextui-org/react';
+import Link from 'next/link';
 
 const HeroWrapper: FC = () => {
-  return (
-    <>
-      <div className="bg-gray-100">
-        <div className="container mx-auto flex flex-col items-center py-12 sm:py-24">
-          <div className="w-11/12 sm:w-2/3 lg:flex justify-center items-center flex-col  mb-5 sm:mb-10">
-            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center text-gray-800 font-black leading-7 md:leading-10">
-              The Freedom to Create the
-              <span className="text-indigo-700">Websites</span>
-              You Want
-            </h1>
-            <p className="mt-5 sm:mt-10 lg:w-10/12 text-gray-400 font-normal text-center text-sm sm:text-lg">
-              A professonal website drives sales. Create a beautiful website to
-              impress and engage new customers and establish your business
-              online{" "}
-            </p>
-          </div>
-          <div className="flex justify-center items-center">
-            <button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 bg-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 lg:text-xl lg:font-bold  rounded text-white px-4 sm:px-10 border border-indigo-700 py-2 sm:py-4 text-sm">
-              Get Started
-            </button>
-            <button className="ml-4 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 bg-transparent transition duration-150 ease-in-out hover:border-indigo-600 lg:text-xl lg:font-bold  hover:text-indigo-600 rounded border border-indigo-700 text-indigo-700 px-4 sm:px-10 py-2 sm:py-4 text-sm">
-              Live Demo
-            </button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className='bg-gray-100'>
+                <div className='container mx-auto flex flex-col items-center py-12 sm:py-24'>
+                    <div className='mb-5 w-11/12 flex-col items-center justify-center sm:mb-10 sm:w-2/3 lg:flex'>
+                        <h1 className='prose prose-h1: text-center text-2xl font-black leading-7 text-gray-800 sm:text-3xl md:text-4xl md:leading-10 lg:text-5xl xl:text-6xl'>
+                            Docu
+                            <span className='text-indigo-700'>Nest</span>
+                        </h1>
+                        <p className='prose prose-p: mt-5 text-center text-sm font-normal text-gray-400 sm:mt-10 sm:text-lg lg:w-10/12'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Temporibus corrupti esse officia in nemo
+                            voluptates distinctio iure blanditiis aspernatur
+                            minus perferendis quasi delectus, dolor quo repellat
+                            omnis? Laudantium, reiciendis labore!
+                        </p>
+                    </div>
+                    <div className='flex items-center justify-center'>
+                        <Button size='lg' variant='solid' color='primary'>
+                            <Link href='/login'>Get Started</Link>
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default HeroWrapper;
