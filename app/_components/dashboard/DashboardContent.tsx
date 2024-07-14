c92ba4724c854a287f1a8504826eb2b2bb9af6cb
Peter Dinis <peterdinis0@pobox.sk>
2024-07-14 14:05:54 +0200

    feat: Update dashboard content

--- app/_components/dashboard/DashboardContent.tsx
+++ app/_components/dashboard/DashboardContent.tsx
@@ -1,10 +1,11 @@
+import { Plus } from "lucide-react";
 import { FC } from "react";
 
 const DashboardContent: FC = () => {
     return (
         <main className='main -ml-48 flex flex-grow flex-col p-4 transition-all duration-150 ease-in md:ml-0'>
         <div className='flex h-full items-center justify-center bg-white text-center text-5xl font-bold shadow-md'>
-            Content
+            <Plus size={70} /> Add new Document
         </div>
     </main>
     )
