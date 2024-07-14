07982b6a3a35247906c91948374b8bd2aa61c0da
Peter Dinis <peterdinis0@pobox.sk>
2024-07-14 10:06:36 +0200

    infra: Formatting files

--- app/_components/hero/HeroServices.tsx
+++ app/_components/hero/HeroServices.tsx
@@ -1,65 +1,70 @@
-import { FC } from "react";
-import { Edit3, Mic, FolderOpen, Users } from "lucide-react";
+import { FC } from 'react';
+import { Edit3, Mic, FolderOpen, Users } from 'lucide-react';
 
 const HeroServices: FC = () => {
     return (
-        <div className="py-16">
-            <div className="xl:container m-auto space-y-16 px-6 text-gray-500 md:px-12">
+        <div className='py-16'>
+            <div className='m-auto space-y-16 px-6 text-gray-500 xl:container md:px-12'>
                 <div>
-                    <h2 className="mt-4 text-2xl font-bold text-gray-800 dark:text-white md:text-4xl">
-                        Enhance Your Note-Taking Experience <br className="sm:block" hidden />
+                    <h2 className='mt-4 text-2xl font-bold text-gray-800 dark:text-white md:text-4xl'>
+                        Enhance Your Note-Taking Experience{' '}
+                        <br className='sm:block' hidden />
                         with AI-Powered Tools
                     </h2>
                 </div>
-                <div className="md:-mx-8 mt-16 grid gap-8 lg:gap-0 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-                    <div className="group relative p-8 rounded-3xl bg-white dark:border-gray-700 dark:bg-gray-800 dark:lg:bg-transparent border border-gray-100 lg:border-transparent hover:border-gray-100 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 shadow-2xl shadow-gray-600/10 lg:shadow-transparent lg:hover:shadow-gray-600/10 sm:gap-8 sm:flex transition duration-300">
-                        <div className="relative">
-                            <Edit3 className="w-10 h-10 text-gray-800 dark:text-white" />
-                            <h3 className="mt-8 mb-4 text-2xl font-semibold text-gray-800 transition dark:text-white">
+                <div className='mt-16 grid gap-8 sm:grid-cols-2 md:-mx-8 lg:grid-cols-3 lg:gap-0 xl:grid-cols-4'>
+                    <div className='group relative rounded-3xl border border-gray-100 bg-white p-8 shadow-2xl shadow-gray-600/10 transition duration-300 hover:border-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 sm:flex sm:gap-8 lg:border-transparent lg:shadow-transparent lg:hover:shadow-gray-600/10 dark:lg:bg-transparent'>
+                        <div className='relative'>
+                            <Edit3 className='h-10 w-10 text-gray-800 dark:text-white' />
+                            <h3 className='mb-4 mt-8 text-2xl font-semibold text-gray-800 transition dark:text-white'>
                                 Intelligent Summarization
                             </h3>
-                            <p className="text-gray-600 dark:text-gray-300">
-                                Automatically generate concise summaries of your notes using advanced AI algorithms.
+                            <p className='text-gray-600 dark:text-gray-300'>
+                                Automatically generate concise summaries of your
+                                notes using advanced AI algorithms.
                             </p>
                         </div>
                     </div>
-                    <div className="group relative p-8 rounded-3xl bg-white dark:border-gray-700 dark:bg-gray-800 dark:lg:bg-transparent border border-gray-100 lg:border-transparent hover:border-gray-100 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 shadow-2xl shadow-gray-600/10 lg:shadow-transparent lg:hover:shadow-gray-600/10 sm:gap-8 sm:flex transition duration-300">
-                        <div className="relative">
-                            <Mic className="w-10 h-10 text-gray-800 dark:text-white" />
-                            <h3 className="mt-8 mb-4 text-2xl font-semibold text-gray-800 transition dark:text-white">
+                    <div className='group relative rounded-3xl border border-gray-100 bg-white p-8 shadow-2xl shadow-gray-600/10 transition duration-300 hover:border-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 sm:flex sm:gap-8 lg:border-transparent lg:shadow-transparent lg:hover:shadow-gray-600/10 dark:lg:bg-transparent'>
+                        <div className='relative'>
+                            <Mic className='h-10 w-10 text-gray-800 dark:text-white' />
+                            <h3 className='mb-4 mt-8 text-2xl font-semibold text-gray-800 transition dark:text-white'>
                                 Voice to Text
                             </h3>
-                            <p className="text-gray-600 dark:text-gray-300">
-                                Convert your spoken words into text, making note-taking faster and more efficient.
+                            <p className='text-gray-600 dark:text-gray-300'>
+                                Convert your spoken words into text, making
+                                note-taking faster and more efficient.
                             </p>
                         </div>
                     </div>
-                    <div className="group relative p-8 rounded-3xl bg-white dark:border-gray-700 dark:bg-gray-800 dark:lg:bg-transparent border border-gray-100 lg:border-transparent hover:border-gray-100 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 shadow-2xl shadow-gray-600/10 lg:shadow-transparent lg:hover:shadow-gray-600/10 sm:gap-8 sm:flex transition duration-300">
-                        <div className="relative">
-                            <FolderOpen className="w-10 h-10 text-gray-800 dark:text-white" />
-                            <h3 className="mt-8 mb-4 text-2xl font-semibold text-gray-800 transition dark:text-white">
+                    <div className='group relative rounded-3xl border border-gray-100 bg-white p-8 shadow-2xl shadow-gray-600/10 transition duration-300 hover:border-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 sm:flex sm:gap-8 lg:border-transparent lg:shadow-transparent lg:hover:shadow-gray-600/10 dark:lg:bg-transparent'>
+                        <div className='relative'>
+                            <FolderOpen className='h-10 w-10 text-gray-800 dark:text-white' />
+                            <h3 className='mb-4 mt-8 text-2xl font-semibold text-gray-800 transition dark:text-white'>
                                 Smart Organization
                             </h3>
-                            <p className="text-gray-600 dark:text-gray-300">
-                                Organize your notes intelligently based on context and relevance using AI categorization.
+                            <p className='text-gray-600 dark:text-gray-300'>
+                                Organize your notes intelligently based on
+                                context and relevance using AI categorization.
                             </p>
                         </div>
                     </div>
-                    <div className="group relative p-8 rounded-3xl bg-white dark:border-gray-700 dark:bg-gray-800 dark:lg:bg-transparent border border-gray-100 lg:border-transparent hover:border-gray-100 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 shadow-2xl shadow-gray-600/10 lg:shadow-transparent lg:hover:shadow-gray-600/10 sm:gap-8 sm:flex transition duration-300">
-                        <div className="relative">
-                            <Users className="w-10 h-10 text-gray-800 dark:text-white" />
-                            <h3 className="mt-8 mb-4 text-2xl font-semibold text-gray-800 transition dark:text-white">
+                    <div className='group relative rounded-3xl border border-gray-100 bg-white p-8 shadow-2xl shadow-gray-600/10 transition duration-300 hover:border-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 sm:flex sm:gap-8 lg:border-transparent lg:shadow-transparent lg:hover:shadow-gray-600/10 dark:lg:bg-transparent'>
+                        <div className='relative'>
+                            <Users className='h-10 w-10 text-gray-800 dark:text-white' />
+                            <h3 className='mb-4 mt-8 text-2xl font-semibold text-gray-800 transition dark:text-white'>
                                 Collaborative Notes
                             </h3>
-                            <p className="text-gray-600 dark:text-gray-300">
-                                Work together with classmates in real-time to create and edit notes seamlessly.
+                            <p className='text-gray-600 dark:text-gray-300'>
+                                Work together with classmates in real-time to
+                                create and edit notes seamlessly.
                             </p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default HeroServices;
--- app/_components/hero/HeroWrapper.tsx
+++ app/_components/hero/HeroWrapper.tsx
@@ -10,11 +10,11 @@ const HeroWrapper: FC = () => {
             <div className='bg-gray-100'>
                 <div className='container mx-auto flex flex-col items-center py-12 sm:py-24'>
                     <div className='mb-5 w-11/12 flex-col items-center justify-center sm:mb-10 sm:w-2/3 lg:flex'>
-                        <h1 className='prose prose-h1: text-center text-2xl font-black leading-7 text-gray-800 sm:text-3xl md:text-4xl md:leading-10 lg:text-5xl xl:text-6xl'>
+                        <h1 className='prose-h1: prose text-center text-2xl font-black leading-7 text-gray-800 sm:text-3xl md:text-4xl md:leading-10 lg:text-5xl xl:text-6xl'>
                             Docu
                             <span className='text-indigo-700'>Nest</span>
                         </h1>
-                        <p className='prose prose-p: mt-5 text-center text-sm font-normal text-gray-400 sm:mt-10 sm:text-lg lg:w-10/12'>
+                        <p className='prose-p: prose mt-5 text-center text-sm font-normal text-gray-400 sm:mt-10 sm:text-lg lg:w-10/12'>
                             Lorem ipsum dolor sit amet consectetur adipisicing
                             elit. Temporibus corrupti esse officia in nemo
                             voluptates distinctio iure blanditiis aspernatur
