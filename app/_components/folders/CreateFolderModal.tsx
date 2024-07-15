d5a71169afcb0557a9eadbb1fbf981de8c79c78f
Peter Dinis <peterdinis0@pobox.sk>
2024-07-15 15:15:46 +0200

    FOO

--- app/_components/folders/CreateFolderModal.tsx
+++ app/_components/folders/CreateFolderModal.tsx
@@ -35,33 +35,11 @@ const CreateFolderModal: FC<ICreateFolderModalProps> = ({
                 <ModalContent>
                     {(onClose) => (
                         <>
-                            <ModalHeader className='flex flex-col gap-1'>
-                                Modal Title
+                            <ModalHeader className='flex text-xl prose prose-h2: flex-col gap-1'>
+                                Create new folder
                             </ModalHeader>
                             <ModalBody>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipiscing elit. Nullam pulvinar risus non
-                                    risus hendrerit venenatis. Pellentesque sit
-                                    amet hendrerit risus, sed porttitor quam.
-                                </p>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipiscing elit. Nullam pulvinar risus non
-                                    risus hendrerit venenatis. Pellentesque sit
-                                    amet hendrerit risus, sed porttitor quam.
-                                </p>
-                                <p>
-                                    Magna exercitation reprehenderit magna aute
-                                    tempor cupidatat consequat elit dolor
-                                    adipisicing. Mollit dolor eiusmod sunt ex
-                                    incididunt cillum quis. Velit duis sit
-                                    officia eiusmod Lorem aliqua enim laboris do
-                                    dolor eiusmod. Et mollit incididunt nisi
-                                    consectetur esse laborum eiusmod pariatur
-                                    proident Lorem eiusmod et. Culpa deserunt
-                                    nostrud ad veniam.
-                                </p>
+                                FORM FOR CREATE NEW FOLDER
                             </ModalBody>
                             <ModalFooter>
                                 <Button
