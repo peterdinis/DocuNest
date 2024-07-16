a0671467eab0c9aa4ff993c06f1df786d0cd01fb
Peter Dinis <peterdinis0@pobox.sk>
2024-07-16 11:11:09 +0200

    FOO

--- app/documents/new/page.tsx
+++ app/documents/new/page.tsx
@@ -1,8 +1,10 @@
 import CreateDocumentForm from '@/app/_components/docs/CreateDocumentForm';
 import { NextPage } from 'next';
-
+/* TODO: Update later */
 const NewDocumentPage: NextPage = () => {
-    return <CreateDocumentForm />;
+    return <CreateDocumentForm onChange={function (value: string): void {
+        throw new Error('Function not implemented.');
+    } } value={''} />;
 };
 
 export default NewDocumentPage;
