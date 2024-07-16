4003799e71e26f353169b32c52744c857e42a453
Peter Dinis <peterdinis0@pobox.sk>
2024-07-16 11:16:36 +0200

    feat: Start writing folders endpoints WIP

---
+++ app/api/folders/route.ts
@@ -0,0 +1,26 @@
+import { getServerSession } from 'next-auth';
+import { NextResponse } from 'next/server';
+import authOptions from '../auth/authOptions';
+import { db } from '@/app/_utils/database';
+
+export async function GET() {
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user) {
+        return NextResponse.json(
+            { error: 'Not authenticated' },
+            { status: 401 },
+        );
+    }
+
+    const allUsersFolders = await db.folder.findMany({
+        where: {
+            userId: session.user.id,
+        },
+    });
+    if (!allUsersFolders) {
+        throw new Error('User does not create any folders');
+    }
+
+    return NextResponse.json(allUsersFolders);
+}
