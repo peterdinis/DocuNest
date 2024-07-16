0bf940330cd923b4d1b91dfdae5455b7b6b7d3fd
Peter Dinis <peterdinis0@pobox.sk>
2024-07-16 10:39:09 +0200

    FOO

---
+++ app/_components/docs/CreateDocumentForm.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const CreateDocumentForm: FC = () => {
+    return (
+        <>
+        aaa
+        </>
+    )
+}
+
+export default CreateDocumentForm;
---
+++ app/_components/folders/AllFoldersWrapper.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const AllFoldersWrapper: FC = () => {
+    return (
+        <>
+        aaa
+        </>
+    )
+}
+
+export default AllFoldersWrapper;
