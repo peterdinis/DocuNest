43b1e62648caec8078cd5e772f91f734bec72d13
Peter Dinis <peterdinis0@pobox.sk>
2024-07-17 12:53:20 +0200

    feat: Using useQuery to displaying data from database

--- app/_components/docs/DocumentsWrapper.tsx
+++ app/_components/docs/DocumentsWrapper.tsx
@@ -4,10 +4,28 @@ import { FC } from 'react';
 import Header from '../shared/Header';
 import DocumentsLists from './DocumentsLists';
 import { Input } from '@nextui-org/input';
-import { Search } from 'lucide-react';
+import { Loader2, Search } from 'lucide-react';
 import AppPagination from '../shared/AppPagination';
+import {
+    useQuery,
+  } from '@tanstack/react-query'
+import { fetchAllDocuments } from '@/app/_store/queries/documentQueries';
 
 const DocumentsWrapper: FC = () => {
+    const {data, isLoading, isError} = useQuery({
+        queryKey: ["myDocuments"],
+        queryFn: () => fetchAllDocuments(),
+        staleTime: Infinity
+    });
+
+    if(isLoading) {
+        return <Loader2 className='animate-spin h-8 w-8' />
+    }
+
+    if(isError) {
+        return <p className='font-bold text-red-700 text-xl'>Something went wrong</p>
+    }
+
     return (
         <>
             <div className='flex justify-center align-top'>
@@ -19,7 +37,7 @@ const DocumentsWrapper: FC = () => {
                 placeholder='Search...'
             />
             <br />
-            <DocumentsLists />
+            <DocumentsLists documentData={data as any} />
             <div className='mt-40 flex justify-center align-top'>
                 <AppPagination total={10} initialPage={1} />
             </div>
