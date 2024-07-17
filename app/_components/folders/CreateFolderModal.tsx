61707320e34add4c74b4d9c99ae1e6e5ce6da4ad
Peter Dinis <peterdinis0@pobox.sk>
2024-07-17 22:42:07 +0200

    feat: Writing code to adding new folders

--- app/_components/folders/CreateFolderModal.tsx
+++ app/_components/folders/CreateFolderModal.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { zodResolver } from '@hookform/resolvers/zod';
 import {
     Modal,
     ModalContent,
@@ -7,34 +8,76 @@ import {
     ModalBody,
     ModalFooter,
     Button,
+    Input,
     useDisclosure,
 } from '@nextui-org/react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { FC } from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import { FolderData, schema } from './schemas';
+import {
+    createNewFolder,
+    ICreateFolder,
+} from '@/app/_store/mutations/folderMutations';
 
 interface ICreateFolderModalProps {
     btnName: string;
 }
 
-const CreateFolderModal: FC<ICreateFolderModalProps> = ({
-    btnName,
-}: ICreateFolderModalProps) => {
+const CreateFolderModal: FC<ICreateFolderModalProps> = ({ btnName }) => {
     const { isOpen, onOpen, onClose } = useDisclosure();
 
-    const handleOpen = () => {
-        onOpen();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+    } = useForm<FolderData>({
+        resolver: zodResolver(schema),
+    });
+
+    const queryClient = useQueryClient();
+
+    const createFolderMut = useMutation({
+        mutationKey: ['createFolder'],
+        mutationFn: async (data: ICreateFolder) => {
+            await createNewFolder(data);
+        },
+
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ['myFolders'],
+            });
+            toast.success('Folder was created');
+            reset();
+            onClose();
+        },
+        onError: () => {
+            toast.error('Error creating folder');
+        },
+    });
+
+    const handleCreateFolder = (data: ICreateFolder) => {
+        createFolderMut.mutate(data);
     };
 
     return (
         <div className='flex flex-wrap gap-3'>
-            <div onClick={() => handleOpen()}>{btnName}</div>
+            <div onClick={onOpen}>{btnName}</div>
             <Modal backdrop={'blur'} isOpen={isOpen} onClose={onClose}>
                 <ModalContent>
-                    {(onClose) => (
-                        <>
-                            <ModalHeader className='prose-h2: prose flex flex-col gap-1 text-xl'>
-                                Create new folder
-                            </ModalHeader>
-                            <ModalBody>FORM FOR CREATE NEW FOLDER</ModalBody>
+                    <>
+                        <ModalHeader className='prose-h2: prose flex flex-col gap-1 text-xl'>
+                            Create new folder
+                        </ModalHeader>
+                        <form onSubmit={handleSubmit(handleCreateFolder)}>
+                            <ModalBody>
+                                <Input
+                                    placeholder='Folder Name'
+                                    {...register('name')}
+                                />
+                            </ModalBody>
                             <ModalFooter>
                                 <Button
                                     color='danger'
@@ -43,12 +86,16 @@ const CreateFolderModal: FC<ICreateFolderModalProps> = ({
                                 >
                                     Close
                                 </Button>
-                                <Button color='primary' onPress={onClose}>
-                                    Action
+                                <Button
+                                    color='primary'
+                                    type='submit'
+                                    isLoading={createFolderMut.isPending}
+                                >
+                                    Create
                                 </Button>
                             </ModalFooter>
-                        </>
-                    )}
+                        </form>
+                    </>
                 </ModalContent>
             </Modal>
         </div>
