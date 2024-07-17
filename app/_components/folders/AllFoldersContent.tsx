1fe88f449d38ce8e246182fbf75ccbd03a9c2d3c
Peter Dinis <peterdinis0@pobox.sk>
2024-07-17 21:12:09 +0200

    FOO

--- app/_components/folders/AllFoldersContent.tsx
+++ app/_components/folders/AllFoldersContent.tsx
@@ -1,14 +1,14 @@
 'use client';
 
 import { FC, useState, useEffect } from 'react';
-import FoldersLists from './FoldersLists';
 import Header from '../shared/Header';
 import { Input } from '@nextui-org/input';
-import { Loader2, Search } from 'lucide-react';
+import { Folder, Loader2, Search } from 'lucide-react';
 import AppPagination from '../shared/AppPagination';
-import Link from 'next/link';
 import {useQuery} from "@tanstack/react-query";
 import { fetchAllFolders } from '@/app/_store/queries/folderQueries';
+import { Badge, Card, CardHeader } from '@nextui-org/react';
+import { Folder as DisplayFolder } from '@prisma/client';
 
 const AllFoldersContent: FC = () => {
     const [searchQuery, setSearchQuery] = useState('');
@@ -45,7 +45,18 @@ const AllFoldersContent: FC = () => {
                 onChange={(e) => setSearchQuery(e.target.value)}
             />
             <br />
-            <FoldersLists />
+            {data && data.map((item: DisplayFolder) => {
+                return (
+                    <div className="mt-5 flex">
+                        <Card className='w-[200px] space-y-5 p-4' radius='lg'>
+                        <div className='h-24 rounded-lg mt-5 flex justify-center align-top'>
+                            <Folder size={50} />
+                        </div>
+                        <Badge color='primary'>{item.name}</Badge>
+                        </Card>
+                    </div>
+                )
+            })}
             <div className='mt-40 flex justify-center align-top'>
                 <AppPagination
                     total={data.totalPages}
