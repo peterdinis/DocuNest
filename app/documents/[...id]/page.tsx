3fae95f1a368dd636a51291f953a22d175e3c7f5
Peter Dinis <peterdinis0@pobox.sk>
2024-07-18 16:55:35 +0200

    feat: Start working on detail pages

---
+++ app/documents/[...id]/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const DocumentDetailPage: NextPage = () => {
+    return (
+        <>
+        ffff
+        </>
+    )
+}
+
+export default DocumentDetailPage;
---
+++ app/folders/[...id]/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const FolderDetailPage: NextPage = () => {
+    return (
+        <>
+        ffff
+        </>
+    )
+}
+
+export default FolderDetailPage;
