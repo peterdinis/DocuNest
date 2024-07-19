b171622d65a58f6d1e3e102d2b28be8f72e518f6
Peter Dinis <peterdinis0@pobox.sk>
2024-07-19 10:49:42 +0200

    refactor: Refactoring documents DONE

--- app/_store/queries/documentQueries.ts
+++ app/_store/queries/documentQueries.ts
@@ -1,12 +1,17 @@
 import axios from "axios";
 
 export const fetchAllPaginatedDocuments = async ({ query = '', page = 1 }) => {
-    const response = await axios.get("/api/docs", {
+    const response = await axios.get("/api/docs/paginated", {
         params: {
             query,
             page
         }
     });
+    return response.data;
+}
+
+export const fetchAllDocuments = async() => {
+    const response = await axios.get("/api/docs");
     return response.data;
 }
 
