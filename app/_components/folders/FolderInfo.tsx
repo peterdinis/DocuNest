04673f6fb1567126ee3da9e78a2e00ab26c37c78
Peter Dinis <peterdinis0@pobox.sk>
2024-07-19 21:26:33 +0200

    fix: Fixing link in Folder Info

--- app/_components/folders/FolderInfo.tsx
+++ app/_components/folders/FolderInfo.tsx
@@ -1,14 +1,12 @@
 'use client';
 
 import { fetchFolderDetail } from '@/app/_store/queries/folderQueries';
-import { Button, ButtonGroup } from '@nextui-org/react';
 import { useQuery } from '@tanstack/react-query';
 import { Loader2 } from 'lucide-react';
 import { useParams } from 'next/navigation';
 import { FC } from 'react';
 import Link from 'next/link';
-import Header from '../shared/Header';
-import { Accordion, AccordionItem } from '@nextui-org/react';
+import { Button, ButtonGroup, Input } from '@nextui-org/react';
 
 const FolderInfo: FC = () => {
     const { id } = useParams<{ id: string }>();
@@ -19,9 +17,6 @@ const FolderInfo: FC = () => {
             return await fetchFolderDetail(id);
         },
     });
-
-    const defaultContent =
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
 
     if (isLoading) {
         return <Loader2 className='h-8 w-8 animate-spin' />;
@@ -43,39 +38,16 @@ const FolderInfo: FC = () => {
 
             <ButtonGroup className='ml-4 mt-6'>
                 <Button variant='solid' color='primary'>
-                    <Link href='/folders'>Go Back</Link>
+                    <Link href='/folders/all'>Go Back</Link>
                 </Button>
             </ButtonGroup>
 
             <hr className='mt-3' />
             <div className='ml-5 mt-5'>
-                <Header text={data.name} />
+                <Input placeholder='Input title' value={data.name} className="max-w-[300px]" />
             </div>
 
-            {/* Later add documents */}
-            <Accordion selectionMode='multiple'>
-                <AccordionItem
-                    key='1'
-                    aria-label='Accordion 1'
-                    title='Accordion 1'
-                >
-                    {defaultContent}
-                </AccordionItem>
-                <AccordionItem
-                    key='2'
-                    aria-label='Accordion 2'
-                    title='Accordion 2'
-                >
-                    {defaultContent}
-                </AccordionItem>
-                <AccordionItem
-                    key='3'
-                    aria-label='Accordion 3'
-                    title='Accordion 3'
-                >
-                    {defaultContent}
-                </AccordionItem>
-            </Accordion>
+            TODO: Display documents later
         </div>
     );
 };
