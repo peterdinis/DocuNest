2cff9e118ec712ca30ff42bd8a5823f6e6a4ae76
Peter Dinis <peterdinis0@pobox.sk>
2024-07-22 12:43:57 +0200

    FOO

--- app/_components/docs/UploadDoc.tsx
+++ app/_components/docs/UploadDoc.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FC, useState } from 'react';
+import { FC, useState, ChangeEvent } from 'react';
 import {
     Modal,
     ModalContent,
@@ -13,64 +13,55 @@ import {
 } from '@nextui-org/react';
 import { Plus } from 'lucide-react';
 import { useSession } from 'next-auth/react';
-import axios from 'axios';
+import { useEdgeStore } from '@/app/_utils/edgestore';
 
 const UploadDoc: FC = () => {
     const { isOpen, onOpen, onOpenChange } = useDisclosure();
     const { data: session } = useSession();
-    const [file, setFile] = useState<File | null>(null);
-
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (event.target.files && event.target.files.length > 0) {
-            setFile(event.target.files[0]);
-        }
-    };
-
-    const handleUpload = async () => {
-        if (file && session) {
-            const formData = new FormData();
-            formData.append('file', file);
-
-            try {
-                const uploadResponse = await axios.post('/api/docs/upload', formData, {
-                    headers: {
-                        'Content-Type': 'multipart/form-data',
-                    },
-                });
-
-                const { url, thumbnailUrl } = uploadResponse.data;
-
-                await axios.post('/api/docs/new', {
-                    userId: session.user.id,
-                    title: file.name,
-                    url,
-                    thumbnailUrl,
-                    contentSize: file.size,
-                });
-
-                setFile(null);
-                onOpenChange();
-            } catch (error) {
-                throw new Error("Something went wrong");
-            }
-        }
-    };
+    const [file, setFile] = useState<any>(null);
+    const { edgestore } = useEdgeStore();
 
     return (
         <>
-            <Button color='default' startContent={<Plus />} onPress={onOpen}>Add document</Button>
+            <Button color='default' startContent={<Plus />} onPress={onOpen}>
+                Add document
+            </Button>
             <Modal isOpen={isOpen} backdrop='blur' onOpenChange={onOpenChange}>
                 <ModalContent>
                     {(onClose) => (
                         <>
-                            <ModalHeader className='flex text-xl mt-5 font-bold prose prose-h2: flex-col gap-1'>
+                            <ModalHeader className='prose-h2: prose mt-5 flex flex-col gap-1 text-xl font-bold'>
                                 Upload Document
                             </ModalHeader>
                             <ModalBody>
                                 <form>
-                                    <Input type='file' onChange={handleFileChange} />
-                                    <Button className='mt-5' color="success" onPress={handleUpload}>
-                                        Upload Document
+                                    <Input
+                                        type='file'
+                                        onChange={(e) => {
+                                            setFile(e.target.files?.[0]);
+                                        }}
+                                    />
+                                    <Button variant='solid' color='success'
+                                        onClick={async () => {
+                                            if (file) {
+                                                const res =
+                                                    await edgestore.publicFiles.upload(
+                                                        {
+                                                            file,
+                                                            onProgressChange: (
+                                                                progress,
+                                                            ) => {
+                                                                // you can use this to show a progress bar
+                                                                console.log(
+                                                                    progress,
+                                                                );
+                                                            },
+                                                        },
+                                                    );
+                                            }
+                                        }}
+                                    >
+                                        Upload
                                     </Button>
                                 </form>
                             </ModalBody>
