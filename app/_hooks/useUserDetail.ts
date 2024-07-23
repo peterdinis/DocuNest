217acd5d1f8c8bd7551cd80209ca3387b76ab385
Peter Dinis <peterdinis0@pobox.sk>
2024-07-23 16:33:09 +0200

    FOO

---
+++ app/_hooks/useUserDetail.ts
@@ -0,0 +1,20 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+import { fetchUserDetail } from '../_store/queries/userQueries';
+
+interface UseUserDetailProps {
+    id: string;
+}
+
+const useUserDetail = ({ id}: UseUserDetailProps) => {
+    return useQuery({
+        queryKey: ['userDetail', id],
+        queryFn: () => fetchUserDetail(id),
+        refetchOnWindowFocus: true,
+        refetchIntervalInBackground: true,
+        refetchOnReconnect: true,
+    });
+};
+
+export default useUserDetail;
