4d3b4902e611ab3343e3318d0f1f227f009c23d3
Peter Dinis <peterdinis0@pobox.sk>
2024-07-26 20:46:39 +0200

    refactor: Add better name for upload router

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -5,30 +5,23 @@ import authOptions from '../auth/authOptions';
 
 const f = createUploadthing();
 
-// FileRouter for your app, can contain multiple FileRoutes
-export const ourFileRouter = {
-    // Define as many FileRoutes as you like, each with a unique routeSlug
-    fileUploader: f({ image: { maxFileSize: '16MB' } })
-        // Set permissions and file types for this FileRoute
+export const uploadRouter = {
+    fileUploader: f({ image: { maxFileSize: '8MB' } })
         .middleware(async ({ req }) => {
-            // This code runs on your server before upload
             const session = await getServerSession(authOptions);
 
             if (!session || !session.user) {
                 throw new UploadThingError('Unauthorized');
             }
-
-            // Whatever is returned here is accessible in onUploadComplete as `metadata`
             return { userId: session.user.id };
         })
         .onUploadComplete(async ({ metadata, file }) => {
             // This code RUNS ON YOUR SERVER after upload
             console.log('Upload complete for userId:', metadata.userId);
             console.log('file url', file.url);
-
-            // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+            /* TODO: Create new document in prisma for specific user */
             return { uploadedBy: metadata.userId };
         }),
 } satisfies FileRouter;
 
-export type OurFileRouter = typeof ourFileRouter;
+export type UploadRouter= typeof uploadRouter;
