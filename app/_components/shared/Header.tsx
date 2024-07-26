aa6890bcb58b23c46bb480c136d47c8573c9ddc1
Peter Dinis <peterdinis0@pobox.sk>
2024-07-26 10:28:10 +0200

    FOO

--- app/_components/shared/Header.tsx
+++ app/_components/shared/Header.tsx
@@ -5,7 +5,7 @@ interface IHeaderProps {
 }
 
 const Header: FC<IHeaderProps> = ({ text }: IHeaderProps) => {
-    return <h1 className='mt-5 ml-3 text-4xl font-bold'>{text}</h1>;
+    return <h1 className='mt-5 ml-3 text-4xl font-bold dark:text-white'>{text}</h1>;
 };
 
 export default Header;
