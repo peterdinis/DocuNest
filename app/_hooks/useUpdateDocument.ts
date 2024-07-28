4093c241bf1df2f5af296930733b1a14ce23b74b
Peter Dinis <peterdinis0@pobox.sk>
2024-07-28 15:19:04 +0200

    feat: Writing new hooks

---
+++ app/_hooks/useUpdateDocument.ts
@@ -0,0 +1,22 @@
+import { useMutation } from '@tanstack/react-query';
+import { updateDocument, UpdateDocumentData } from '@/app/_store/mutations/documentMutations';
+import { queryClient } from '@/app/_store/queryClient';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/navigation';
+
+export const useUpdateDocument = (id: string) => {
+    const router = useRouter();
+
+    return useMutation({
+        mutationKey: ['updateDocument', id],
+        mutationFn: (data: UpdateDocumentData) => updateDocument(id, data),
+        onSuccess: (updatedData) => {
+            toast.success('Document was edited');
+            queryClient.invalidateQueries({ queryKey: ['docDetail', id] });
+            router.push('/dashboard');
+        },
+        onError: () => {
+            toast.error('Document was not edited');
+        },
+    });
+};
