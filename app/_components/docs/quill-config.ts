22668aab2671c9c5f9f12d12739cdcb794a7744f
Peter Dinis <peterdinis0@pobox.sk>
2024-07-30 20:55:30 +0200

    feat: Update quill-config

--- app/_components/docs/quill-config.ts
+++ app/_components/docs/quill-config.ts
@@ -1,6 +1,6 @@
 export const modules = {
     toolbar: [
-        [{ header: [1, 2, 3, 4, 5, false] }],
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
         [{ font: [] }],
         ['bold', 'italic', 'underline', 'strike', 'blockquote'],
         [
