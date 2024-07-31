61b8c55060d5f526fa8c3658440efe771c5deb0e
Peter Dinis <peterdinis0@pobox.sk>
2024-07-31 14:12:58 +0200

    FOO

--- app/_components/docs/editor/QuillEditor.tsx
+++ app/_components/docs/editor/QuillEditor.tsx
@@ -1,33 +1,18 @@
+"use client"
+
 import dynamic from 'next/dynamic';
 import { FC, useMemo } from 'react';
 import { Quill } from 'react-quill';
 import MagicUrl from 'quill-magic-url';
+import QuillCursors from 'quill-cursors';
 import 'react-quill/dist/quill.snow.css';
 import 'quill-paste-smart';
 import Loading from '../../shared/Loading';
 import { modules, formats } from './quill-config';
-import QuillCursors from 'quill-cursors';
-import * as QuillTableUI from 'quill-table-ui';
-import ImageCompress from 'quill-image-compress';
 
-Quill.register('modules/imageCompress', ImageCompress);
-Quill.register(
-    {
-        'modules/tableUI': QuillTableUI.default,
-    },
-    true,
-);
+// Register multiple modules with Quill
 Quill.register('modules/magicUrl', MagicUrl);
 Quill.register('modules/cursors', QuillCursors);
-
-const ReactQuill = useMemo(
-    () =>
-        dynamic(() => import('react-quill'), {
-            ssr: false,
-            loading: () => <Loading />,
-        }),
-    [],
-);
 
 interface QuillEditorProps {
     value: string;
@@ -36,9 +21,11 @@ interface QuillEditorProps {
 }
 
 const QuillEditor: FC<QuillEditorProps> = ({ value, readOnly, onChange }) => {
+    const ReactQuill = useMemo(() => dynamic(() => import('react-quill'), { ssr: false, loading: () => <Loading /> }), []);
+
     return (
         <ReactQuill
-            theme='snow'
+            theme="snow"
             className={`mb-6 mt-10 h-[100vh] whitespace-pre-wrap ${readOnly ? 'ql-disabled' : ''}`}
             modules={modules}
             formats={formats}
