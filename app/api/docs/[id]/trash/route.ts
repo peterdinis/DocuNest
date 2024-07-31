6a574f2dc2ff8b0b9b217e79f3269c6a01c88d21
Peter Dinis <peterdinis0@pobox.sk>
2024-07-31 11:54:49 +0200

    feat: New endpoint for trash

---
+++ app/api/docs/[id]/trash/route.ts
@@ -0,0 +1,37 @@
+import { db } from "@/app/_utils/database";
+import authOptions from "@/app/api/auth/authOptions";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PUT(request: NextRequest) {
+    const url = new URL(request.url);
+    const id = url.pathname.split('/').pop();
+
+    if (!id) {
+        return NextResponse.json(
+            { error: 'Missing id parameter' },
+            { status: 400 },
+        );
+    }
+
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user) {
+        return NextResponse.json(
+            { error: 'Not authenticated' },
+            { status: 401 },
+        );
+    }
+
+    await db.document.update({
+        where: {
+            id,
+            userId: session.user.id,
+        },
+        data: {
+            inTrash: false
+        },
+    });
+
+    return new NextResponse('Remove from trash and deleted', { status: 200 });
+}
