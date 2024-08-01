d814e456c468834d624b18f7b2cf08282b04046d
Peter Dinis <peterdinis0@pobox.sk>
2024-08-01 10:06:13 +0200

    FOO

---
+++ app/_hooks/shared/useSearch.ts
@@ -0,0 +1,16 @@
+"use client"
+
+import { fetchSearchResults } from "@/app/_store/queries/searchQueries"
+import { useQuery } from "@tanstack/react-query"
+
+export const useSearch = (searchQuery: string) => {
+    return useQuery({
+        queryKey: ["searchResults", searchQuery],
+        queryFn: () => fetchSearchResults(searchQuery),
+        enabled: true,
+        staleTime: Infinity,
+        refetchIntervalInBackground: true,
+        refetchOnWindowFocus: true,
+        refetchOnMount: true,
+    })
+}
