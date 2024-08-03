4c43498293c4f0f49219c095e72d4e204b6d747d
Peter Dinis <peterdinis0@pobox.sk>
2024-08-03 16:30:47 +0200

    feat: New File table

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -2,6 +2,7 @@ import { getServerSession } from 'next-auth';
 import { createUploadthing, type FileRouter } from 'uploadthing/next';
 import { UploadThingError } from 'uploadthing/server';
 import authOptions from '../auth/authOptions';
+import { db } from '@/app/_utils/database';
 
 const f = createUploadthing();
 
@@ -16,12 +17,6 @@ export const uploadRouter = {
         })
         .onUploadComplete(async ({ metadata, file }) => {
             try {
-                const session = await getServerSession(authOptions);
-
-                if (!session || !session.user) {
-                    console.error('Unauthorized user at onUploadComplete');
-                    return; // Early exit instead of throwing an error
-                }
                 return { uploadedBy: metadata.userId };
             } catch (error) {
                 console.error('Error in onUploadComplete:', error);
