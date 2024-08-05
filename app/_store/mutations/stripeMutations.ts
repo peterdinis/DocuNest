e613becff7d3ef0f2c9f7ef917d680568c7a913a
Peter Dinis <peterdinis0@pobox.sk>
2024-08-05 17:30:34 +0200

    FOO

--- app/_store/mutations/stripeMutations.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const handleSubscriptMut = async(priceId: string) => {
-    const response = await axios.post("/api/stripe/subscription", priceId);
-    return response.data;
-}
